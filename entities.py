eda7543d333d287a5b608f7b5197e6d8721b4272
Orlando A. Courrau <ocourrau@yahoo.com>
2017-03-18 10:25:02 -0600

    Agregando nuevo codigo de suma resta multiplicacion division en entities.py

--- entities.py
+++ entities.py
@@ -1,7 +1,10 @@
 class Calculadora:
-
+    valor1 = 0
+    valor2 = 0
+    resultado = 0
+    
     def suma(self):
-        pass
+        self.resultado = self.valor1 + self.valor2
 
     def resta(self):
         pass
