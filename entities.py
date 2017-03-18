c5d46753aa6cbc9cceb166b105e122367ab5fd9f
Orlando A. Courrau <ocourrau@yahoo.com>
2017-03-18 09:43:35 -0600

    Adding entities.py code file

---
+++ entities.py
@@ -0,0 +1,13 @@
+class Calculadora:
+
+    def suma(self):
+        pass
+
+    def resta(self):
+        pass
+
+    def multiplicacion(self):
+        pass
+
+    def division(self):
+        pass
