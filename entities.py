c2338f9a298bdffe42665b7616a73851724150f0
David <david.mora6162@gmail.com>
2017-03-18 10:43:38 -0600

    Adding new sum functionality to Calculadora class

--- entities.py
+++ entities.py
@@ -1,10 +1,11 @@
 class Calculadora:
-    valor1 = 0
-    valor2 = 0
-    resultado = 0
-    
-    def suma(self):
-        self.resultado = self.valor1 + self.valor2
+
+    def __init__(self):
+        pass
+
+    def suma(self,num1,num2):
+        print(int(num1)+int(num2))
+        #return int(num1) + int(num2)
 
     def resta(self):
         pass
