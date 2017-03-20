7c266c2500bf317685867eeb4cdcd4bafd432b90
Orlando A. Courrau <ocourrau@yahoo.com>
2017-03-20 12:38:33 -0600

    Enabling return logic in entites.py file instead

--- entities.py
+++ entities.py
@@ -4,17 +4,17 @@ class Calculadora:
         pass
 
     def suma(self,num1,num2):
-        print(int(num1)+int(num2))
-        #return int(num1) + int(num2)
+        #print(int(num1)+int(num2))
+        return int(num1) + int(num2)
 
     def resta(self,num1,num2):
-        print(int(num1)-int(num2))
-        #return int(num1) - int(num2)
+        #print(int(num1)-int(num2))
+        return int(num1) - int(num2)
 
     def multiplicacion(self,num1,num2):
-        print(int(num1)*int(num2))
-        #return int(num1) * int(num2)
+        #print(int(num1)*int(num2))
+        return int(num1) * int(num2)
 
     def division(self,num1,num2):
-        print(int(num1)//int(num2))
-        #return int(num1) / int(num2)
+        #print(int(num1)//int(num2))
+        return int(num1) // int(num2)
