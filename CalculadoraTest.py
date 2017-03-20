506b0980c2e73ff48174dbd37d919d0d53777046
Orlando A. Courrau <ocourrau@yahoo.com>
2017-03-20 10:24:54 -0600

    Updating verbiage to know what math functionality we arr using in CalculadoraTest.py file

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -8,23 +8,23 @@ class CalculadoraTest(unittest.TestCase):
         pass
 
     def testSum(self):
-        num1 = int(input('Ingrese un numero '))
-        num2 = int(input('Ingrese otro numero '))
+        num1 = int(input('Ingrese un numero a sumar '))
+        num2 = int(input('Ingrese otro numero a sumar '))
         self.assertEqual(calc.suma(num1,num2),4)
 
     def testResta(self):
-        num1 = int(input('Ingrese un numero '))
-        num2 = int(input('Ingrese un numero '))
+        num1 = int(input('Ingrese un numero a restar '))
+        num2 = int(input('Ingrese un numero a restar '))
         self.assertEqual(calc.resta(num1,num2),6)
 
     def testMultiplicacion(self):
-        num1 = int(input('Ingrese un numero '))
-        num2 = int(input('Ingrese un numero '))
+        num1 = int(input('Ingrese un numero a multiplicar '))
+        num2 = int(input('Ingrese un numero a multiplicar '))
         self.assertEqual(calc.multiplicacion(num1,num2),8)
 
     def testDivision(self):
-        num1 = int(input('Ingrese un numero '))
-        num2 = int(input('Ingrese un numero '))
+        num1 = int(input('Ingrese un numero a dividir '))
+        num2 = int(input('Ingrese un numero a dividir '))
         self.assertEqual(calc.division(num1,num2),10)
 
 if __name__ == '__main__':
