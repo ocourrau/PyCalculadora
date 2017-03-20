5c9d31935fdc1c0f42db19494efce3d6c21e3aa1
Orlando A. Courrau <ocourrau@yahoo.com>
2017-03-20 10:16:31 -0600

    Adding additional math functionality to CalculadoraTest.py file

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -12,5 +12,20 @@ class CalculadoraTest(unittest.TestCase):
         num2 = int(input('Ingrese otro numero '))
         self.assertEqual(calc.suma(num1,num2),4)
 
+    def testResta(self):
+        num1 = int(input('Ingrese un numero '))
+        num2 = int(input('Ingrese un numero '))
+        self.assertEqual(calc.resta(num1,num2),6)
+
+    def testMultiplicacion(self):
+        num1 = int(input('Ingrese un numero '))
+        num2 = int(input('Ingrese un numero '))
+        self.assertEqual(calc.multiplicacion(num1,num2),8)
+
+    def testDivision(self):
+        num1 = int(input('Ingrese un numero '))
+        num2 = int(input('Ingrese un numero '))
+        self.assertEqual(calc.division(num1,num2),10)
+
 if __name__ == '__main__':
     unittest.main()
